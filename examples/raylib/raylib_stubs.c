e530e0546c3022f956550431034bf75612817668
maltasea <hot.crab7062@fastmail.com>
2026-05-19 17:34:40 +0200

    Remove raylib example (moved out of repo)

--- examples/raylib/raylib_stubs.c
+++
@@ -1,9 +0,0 @@
-#include <caml/mlvalues.h>
-CAMLprim value rl_init_window(value a,value b,value c) { (void)a;(void)b;(void)c; return Val_unit; }
-CAMLprim value rl_window_should_close(value v) { (void)v; return Val_bool(0); }
-CAMLprim value rl_begin(value v) { (void)v; return Val_unit; }
-CAMLprim value rl_end(value v) { (void)v; return Val_unit; }
-CAMLprim value rl_clear(value a,value b,value c,value d) { (void)a;(void)b;(void)c;(void)d; return Val_unit; }
-CAMLprim value rl_draw_rect(value a,value b,value c,value d,value e,value f,value g,value h) { (void)a;(void)b;(void)c;(void)d;(void)e;(void)f;(void)g;(void)h; return Val_unit; }
-CAMLprim value rl_draw_text(value a,value b,value c,value d,value e,value f,value g,value h) { (void)a;(void)b;(void)c;(void)d;(void)e;(void)f;(void)g;(void)h; return Val_unit; }
-CAMLprim value rl_is_key_down(value v) { (void)v; return Val_bool(0); }
